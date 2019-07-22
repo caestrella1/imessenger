214179081fe6121f43a9625ab1395b9a367d44cc
Carlos Estrada <cestrrada@gmail.com>
2019-07-22 13:18:54 -0600

    Create MessageList dummy data

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,8 +1,38 @@
 import React from 'react';
 
+const DUMMY_DATA = [
+  {
+    senderId: 'person1',
+    message: 'Hello world!'
+  },
+  {
+    senderId: 'carlos',
+    message: 'Hello world! (2)'
+  },
+  {
+    senderId: 'jack',
+    message: 'Testing jack'
+  },
+  {
+    senderId: 'charlie',
+    message: 'testing charlie'
+  },
+];
+
 class MessageList extends React.Component {
   render() {
-    return null;
+    return (
+      <div className="message-list">
+        {DUMMY_DATA.map((message, index) => {
+          return (
+            <div key={index} className="message">
+              <div className="message-username">{message.senderId}</div>
+              <div className="message-text">{message.message}</div>
+            </div>
+          );
+        })}
+      </div>
+    );
   }
 }
 

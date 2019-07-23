f78c796cf0605cc5ff8170944653469359d578ba
Carlos Estrada <cestrrada@gmail.com>
2019-07-22 18:47:00 -0600

    Get rid of dummy data

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,36 +1,20 @@
 import React from 'react';
 
-const DUMMY_DATA = [
-    {
-        senderId: 'person1',
-        text: 'Hello world!'
-    },
-    {
-        senderId: 'carlos',
-        text: 'Hello world! (2)'
-    },
-    {
-        senderId: 'jack',
-        text: 'Testing jack'
-    },
-    {
-        senderId: 'charlie',
-        text: 'testing charlie'
-    },
-];
-
 class MessageList extends React.Component {
     render() {
         return (
             <div className="message-list">
-                {DUMMY_DATA.map((message, index) => {
+                {this.props.messages.map((message, index) => {
                     return (
                         <div key={index} className="message">
                             <div className="message-username">
                                 {message.senderId}
                             </div>
                             <div className="message-text">
-                                {message.text}
+                                {message.parts[0].payload.content}
+                            </div>
+                            <div className="message-timestamp">
+                                Sent {message.createdAt}
                             </div>
                         </div>
                     );

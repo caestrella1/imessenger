6723c2f4a22a3c02df68f479548937e7cd272e46
Carlos Estrada <cestrrada@gmail.com>
2019-07-22 19:08:55 -0600

    Set up form for sending messages

--- src/components/SendMessageForm.js
+++ src/components/SendMessageForm.js
@@ -1,9 +1,40 @@
 import React from 'react';
 
 class SendMessageForm extends React.Component {
-  render() {
-    return null;
-  }
+
+    constructor() {
+        super();
+        this.state = {
+            message: ""
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(e) {
+        this.setState({
+            message: e.target.value
+        })
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        console.log(this.state.message);
+    }
+
+    render() {
+        return (
+            <form
+                onSubmit={this.handleSubmit}
+                className="send-message">
+                <input
+                    onChange={this.handleChange}
+                    value={this.state.message}
+                    placeholder="Send Message"
+                    type="text"/>
+            </form>
+        );
+    }
 }
 
 export default SendMessageForm;

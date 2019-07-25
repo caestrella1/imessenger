590ac19f780ec2329d2a78fa6402a5dca6f3ea50
Carlos Estrada <cestrrada@gmail.com>
2019-07-24 20:53:24 -0600

    Display time since last message and room privacy labels

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -13,9 +13,22 @@ class RoomList extends React.Component {
                 <ul className="room-list">
                     {orderedRooms.map(room => {
                         const active = this.props.roomId === room.id ? "active" : "";
+                        const isPrivate = room.isPrivate ? "Private" : "Public";
+                        const showUnread = room.unreadCount > 0 ? "show" : "";
+                        const lastMessage = new Date(room.lastMessageAt);
+
                         return (
                             <li key={room.id} className={"room-name " + active}>
-                                <button onClick={() => this.props.subscribeToRoom(room.id)}> # {room.name} {room.isPrivate ? "(Private)" : ""}</button>
+                                <button onClick={() => this.props.subscribeToRoom(room.id)}>
+                                    <span className="is-private">{isPrivate}</span>
+                                    {room.name}
+                                    <span className={"unread-messages " + showUnread}>
+                                        {room.unreadCount}
+                                    </span>
+                                    <span className="last-message">
+                                        { timeDifference(new Date(), lastMessage) }
+                                    </span>
+                                </button>
                             </li>
                         );
                     })}
@@ -23,6 +36,46 @@ class RoomList extends React.Component {
             </div>
         );
     }
+}
+
+function timeDifference(current, previous) {
+    var minute = 60 * 1000;
+    var hour = minute * 60;
+    var day = hour * 24;
+    var month = day * 30;
+    var year = day * 365;
+
+    var elapsed = current - previous;
+    var resultString;
+
+    if (isNaN(previous)) {
+        return 'No messages';
+    }
+    else if (elapsed < minute) {
+        resultString = 'Just now';
+    }
+    else if (elapsed < hour) {
+        let num = Math.round(elapsed/minute);
+        (num > 1) ? resultString = num + ' minutes ago' : resultString = num + ' minute ago';
+    }
+    else if (elapsed < day) {
+        let num = Math.round(elapsed/hour);
+        num > 1 ? resultString = num + ' hours ago' : resultString = num + ' hour ago';
+    }
+    else if (elapsed < month) {
+        let num = Math.round(elapsed/day);
+        num > 1 ? resultString = num + ' days ago' : resultString = num + ' day ago';
+    }
+    else if (elapsed < year) {
+        let num = Math.round(elapsed/month);
+        num > 1 ? resultString = num + ' months ago' : resultString = num + ' month ago';
+    }
+    else {
+        let num = Math.round(elapsed/year);
+        num > 1 ? resultString = num + ' years ago' : resultString = num + ' year ago';
+    }
+
+    return resultString;
 }
 
 export default RoomList;

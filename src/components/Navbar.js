9be7524fdfdf638e8980d1385abe4f977a487210
Carlos Estrada <cestrrada@gmail.com>
2019-08-02 18:37:48 -0600

    Add app name and icon to navbar.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons'
+import { faSun, faMoon, faComments } from '@fortawesome/free-solid-svg-icons'
 
 class Navbar extends React.Component {
 
     render() {
         return (
             <div className="navbar">
+                <h1 id="app-title">
+                    <FontAwesomeIcon icon={faComments}/>
+                    iMessenger
+                </h1>
                 <button id="toggle-theme" onClick={this.props.toggleDarkMode}>
                     <FontAwesomeIcon icon={this.props.isDark ? faSun : faMoon}/>
                 </button>

a8d8d56374b662f1697b59e998ec878d2250c6cb
Carlos Estrada <me@caestrada.com>
2020-01-13 19:21:28 -0800

    Updates

---
+++ precache-manifest.9c15f6170d486e4bdbad7f9cb3f26fba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f24c898ffb9d7241da9e0e7ff431df46",
+    "url": "/imessenger/index.html"
+  },
+  {
+    "revision": "fcfa24362d1fff2a42e8",
+    "url": "/imessenger/static/css/2.b0f64131.chunk.css"
+  },
+  {
+    "revision": "385e6c8455af62b6778d",
+    "url": "/imessenger/static/css/main.7829bd51.chunk.css"
+  },
+  {
+    "revision": "fcfa24362d1fff2a42e8",
+    "url": "/imessenger/static/js/2.867ec036.chunk.js"
+  },
+  {
+    "revision": "8625433f1d2fd52664a63c97ae57ab43",
+    "url": "/imessenger/static/js/2.867ec036.chunk.js.LICENSE"
+  },
+  {
+    "revision": "385e6c8455af62b6778d",
+    "url": "/imessenger/static/js/main.f4d67d7c.chunk.js"
+  },
+  {
+    "revision": "da9d8012b78620749996",
+    "url": "/imessenger/static/js/runtime-main.685f0f17.js"
+  }
+]);
